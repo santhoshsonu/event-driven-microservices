259cd19e62503ee463d791f10f9f8d497f50b353
SanthoshKumar <santhosheshwaramma@gmail.com>
2023-01-12 00:41:44 +0530

    Implemented JCE encryption

---
+++ config-server/src/main/java/com/microservices/eventdriven/config/server/config/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.microservices.eventdriven.config.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf().ignoringRequestMatchers("/encrypt/**").ignoringRequestMatchers("/decrypt/**");
+    return http.build();
+  }
+}
